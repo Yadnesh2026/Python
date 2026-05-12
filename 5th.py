6d46ac6b52d968c962d015fce7e3269c5c6b583f
Vedant <vvidulkar@gmail.com>
2026-05-12 22:57:35 +0530

    Sets method complete

--- 5th.py
+++ 5th.py
@@ -17,21 +17,41 @@ while number<=100:
 #     n+=1
 
 #Qs4
-nums=[1,4,9,16,25,36,49,64,81,100]
-idx =0
+# nums=[1,4,9,16,25,36,49,64,81,100]
+# idx =0
 
-while idx < len(nums):
-    print(nums[idx])
-    idx +=1
+# while idx < len(nums):
+#     print(nums[idx])
+#     idx +=1
 
-#Qs5
-num2 = (1,4,9,16,25,36,49,64,81,100);
-i=0
-x=64
+# #Qs5
+# num2 = (1,4,9,16,25,36,49,64,81,100);
+# i=0
+# x=64
 
-while i < len(num2):
-    if(num2[i]== x):
-        print("Found",i)
-    else:
-        print("finding")
-    i +=1
+# while i < len(num2):
+#     if(num2[i]== x):
+#         print("Found",i)
+#     i +=1
+
+# #Break nd Continue
+# j=0
+
+# while(j<=5):
+#     if(j ==3):
+#         j +=1
+#         continue
+#     print(j)
+#     j +=1
+
+# #Qs6
+# num3 = (1,4,9,16,25,36,49,64,81,100);
+# a=0
+# x=25
+
+# while a< len(num3):
+#     if(num3[a] == 25):
+#         print("Found",a)
+#     a +=1
+
+
