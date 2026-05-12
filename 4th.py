c6b14f3a494d7649bec2b14f015caf5e77b97692
Vedant <vvidulkar@gmail.com>
2026-05-12 17:25:50 +0530

    dict

---
+++ 4th.py
@@ -0,0 +1,11 @@
+#Dictionary in Python - Are used to store data value in key:value pairs
+dict={
+    "name":"Vedant",
+    "cgpa":9.37,
+    "Branch":"BTech ITDS",
+    "is_adult":True,
+    "subjects":["python","C","Java","C++"],
+    "topics":("dict","Set")
+}
+
+print(dict)
