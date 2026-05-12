f8afa658058871f08bbc0c8a839824a50c91a072
Vedant <vvidulkar@gmail.com>
2026-05-12 22:24:52 +0530

    Sets method complete

---
+++ 5th.py
@@ -0,0 +1,15 @@
+#Loops in Python
+
+#While Loop
+number = 1
+
+while number<=100:
+    # print(number)
+    number+=1
+
+n=1
+i= int(input("Enter the number :"))
+
+while (n<=10):
+    print(n*i)
+    n+=1
