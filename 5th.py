bce9a5501b52c8155e491271a5dcc92986a21ca1
Vedant <vvidulkar@gmail.com>
2026-05-12 22:36:25 +0530

    Sets method complete

--- 5th.py
+++ 5th.py
@@ -7,9 +7,19 @@ while number<=100:
     # print(number)
     number+=1
 
-n=1
-i= int(input("Enter the number :"))
 
-while (n<=10):
-    print(n*i)
-    n+=1
+# #Print the table
+# n=1
+# i= int(input("Enter the number :"))
+
+# while (n<=10):
+#     print(n*i)
+#     n+=1
+
+#Qs4
+nums=[1,4,9,16,25,36,49,64,81,100]
+idx =0
+
+while idx < len(nums):
+    print(nums[idx])
+    idx +=1
