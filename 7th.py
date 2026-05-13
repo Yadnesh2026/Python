d41f20ede92db169822d5e89d8eeec78d0e4b286
Vedant <vvidulkar@gmail.com>
2026-05-13 22:39:45 +0530

    7th

--- 7th.py
+++ 7th.py
@@ -4,16 +4,37 @@
 # f = open("filename","mode") #mode - read mode, write mode , deafult parametr is read
 
 # r - readfile ,
-# w - open for writing again,
+# w - Create a file and writing again,
 # x - new file, 
 # a - append the data,
 # b - binary mode, 
 # t - for open text file, 
 # + - open a disk file for updating(reading and writing)
 
-f= open("demo.txt","r") #read mode
+# f= open("demo.txt","r") #read mode
 
-data = f.read() # To see the file data
-print(data)
-print(type(data))
-f.close() #Close the file
+# data = f.read() # To see the file data
+# print(data)
+# print(type(data))
+# # f.close() #Close the file
+
+# data2 = f.readline() #read Line by line
+# print(data2)
+
+#Write in the file - write the data again new
+# f= open("demo.txt","w")
+# f.write("I want to learn Javscript tomm")
+# f.close()
+
+# # Append the data in same file
+# f= open("demo1.txt","a")
+# f.write("\n Added the new data here") #\n add in next line
+# f.close()
+
+#r+ mode - over write in the start of file
+f = open("demo.txt","r+")
+f.write("abc;kl;")
+print(f.read())
+f.close()
+
+
