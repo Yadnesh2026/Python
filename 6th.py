cb3d76f31654beffefe1229444f370dce5dbffe4
Vedant <vvidulkar@gmail.com>
2026-05-13 14:54:10 +0530

    Function Example total

--- 6th.py
+++ 6th.py
@@ -31,17 +31,29 @@
 # print(print2(heros))
 
 #Qs3 - Factorial of n
-def factorial(n):
-    fact = 1
-    for i in range(1,n+1):
-        fact *=i
-    print(fact)
+# def factorial(n):
+#     fact = 1
+#     for i in range(1,n+1):
+#         fact *=i
+#     print(fact)
 
-print(factorial(8))
+# print(factorial(8))
 
 #Qs4 - Convert USD TO INR
-def converter(usd_value):
-    inr_value = usd_value * 83
-    print(usd_value,"USD =", inr_value)
+# def converter(usd_value):
+#     inr_value = usd_value * 83
+#     print(usd_value,"USD =", inr_value)
 
-converter(9)
+# converter(9)
+
+# user = int(input("Enter the Number :"))
+# def check(user2):
+#     if(user2 % 2==0):
+#         print("ODD")
+#     else:
+#         print("EVEN")
+
+# check(user);
+
+#Recursion - When a function calls itself repeatedly 
+
