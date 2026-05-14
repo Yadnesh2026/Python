334bc00000c4ab96266775d629ad43be94c7481e
Vedant <vvidulkar@gmail.com>
2026-05-14 21:10:40 +0530

    OPPs

--- 8th.py
+++ 8th.py
@@ -1,1 +1,9 @@
 #OOPs - Object Oritended Programming - To map with real world scenarios we started using objects i code
+
+#1. Classand Object - Class is a blueprint for creating a objects
+class Student: #creating class
+    name="karan"
+
+s1 =Student() #Object (insatnce)
+print(s1)
+
