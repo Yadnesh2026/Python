b7f4d3c537252c643a4f8c0db4ab13c5955e62b3
Vedant <vvidulkar@gmail.com>
2026-05-16 12:12:20 +0530

    Make the new file for Practice

---
+++ Practice.py
@@ -0,0 +1,95 @@
+#Ex1
+class Laptop:
+    def __init__(self,brand,ram,price):
+        self.brand = brand
+        self.ram = ram
+        self.price = price
+
+    def showDetails(self):
+        print("Brand",self.brand)
+        print("RAM",self.ram)
+        print("Price",self.price)
+
+lappy = Laptop("Hp","16GB",65000)
+lappy.showDetails()
+
+#Ex2
+class Student:
+    def __init__(self,name,marks1,marks2,marks3):
+        self.name = name
+        self.marks1 = marks1
+        self.marks2 = marks2
+        self.marks3 = marks3
+
+    def Avg(self):
+        avg = (self.marks1 + self.marks2 + self.marks3)/3
+        print("The Avg of the Marks are",avg)
+
+s = Student("vedant",97,89,78)
+s.Avg()
+
+#Ex3
+class Rectangle:
+    def __init__(self,len,width):
+        self.len = len
+        self.width = width
+
+    def Area(self):
+        area = self.len * self.width
+        print("Area",area)
+
+    def Perimeter(self):
+        peri = 2 * (self.len +self.width)
+        print("Perimeter",peri)
+
+r = Rectangle(5,10)
+r.Area()
+r.Perimeter()
+
+# #Ex4
+# class BankAcc:
+#     def __init__(self,name,balance):
+#         self.name = name
+#         self.bal = balance
+
+#     def Deposit(self,debited):
+#         self.debited -= self.bal
+#         print("Deposit Amount",debited)
+
+#     def Credit(self,Credited):
+#         self.credited += self.debited
+#         print("Credited Amout",Credited)
+
+
+# b = BankAcc("Vedant",5000)
+# b.Deposit(2000)
+
+#Ex5
+class Mobile:
+    def __init__(self,battery =100,power ="off"):
+        self.battery =battery
+        self.power = power
+
+    def switchOn(self):
+        if(self.power == "off"):
+            self.power = "Onn"
+            print("Mobile switched ON")
+    
+    def switchOff(self):
+        if(self.power == "Onn"):
+            self.power ="Off"
+            print("Mobile switched OFF")
+
+    def charging(self):
+        print("The phone is charging")
+
+
+m = Mobile(100,"off")
+m.switchOn()
+m.switchOff()
+
+
+
+
+        
+
